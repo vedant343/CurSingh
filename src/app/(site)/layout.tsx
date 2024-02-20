f192bd0b6ba2e8a6ce653f6d48817e0cc8d743d0
Vedant Bhalerao <vedantbhalerao343@gmail.com>
2024-02-20 22:42:11 +0530

    final commit of day

---
+++ src/app/(site)/layout.tsx
@@ -0,0 +1,22 @@
+import React from "react";
+
+const HomePageLayout = () => {
+  return (
+    <section>
+      <div
+        className="overflow-hidden
+        px-4
+        sm:px-4
+        mt-10
+        sm:flex
+        sm:flex-col
+        gap-4
+        md:justify-center
+        md:items-center
+        "
+      ></div>
+    </section>
+  );
+};
+
+export default HomePageLayout;
