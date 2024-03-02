4d148e71e0afe1382d30ebfd124aec169dc50a26
Vedant Bhalerao <vedantbhalerao343@gmail.com>
2024-03-02 14:41:35 +0530

    Middle section of LP added

--- src/app/(site)/page.tsx
+++ src/app/(site)/page.tsx
@@ -3,7 +3,8 @@ import React from "react";
 import { Button } from "@/components/ui/button";
 import Image from "next/image";
 import Banner from "../../../public/appBanner.png";
-
+import { CLIENTS, PRICING_CARDS, PRICING_PLANS, USERS } from "@/lib/constants";
+import Cal from "../../../public/cal.png";
 const HomePage = () => {
   return (
     <>
@@ -69,20 +70,112 @@ const HomePage = () => {
             left-0
             right-0
             absolute
-            z-10
-          "
+            z-10"
           ></div>
         </div>
       </section>
       <section className="relative">
-        <div className="overflow-hidden
+        <div
+          className="overflow-hidden
+          flex
+          after:content['']
+          after:dark:from-brand-dark
+          after:to-transparent
+          after:from-background
+          after:bg-gradient-to-l
+          after:right-0
+          after:bottom-0
+          after:top-0
+          after:w-20
+          after:z-10
+          after:absolute
+
+          before:content['']
+          before:dark:from-brand-dark
+          before:to-transparent
+          before:from-background
+          before:bg-gradient-to-r
+          before:left-0
+          before:top-0
+          before:bottom-0
+          before:w-20
+          before:z-10
+          before:absolute
+        "
+        >
+          {[...Array(2)].map((arr) => (
+            <div
+              key={arr}
+              className="flex
+                flex-nowrap
+                animate-slide
+          "
+            >
+              {CLIENTS.map((client) => (
+                <div
+                  key={client.alt}
+                  className=" relative
+                    w-[200px]
+                    m-20
+                    shrink-0
+                    flex
+                    items-center
+                  "
+                >
+                  <Image
+                    src={client.logo}
+                    alt={client.alt}
+                    width={200}
+                    className="object-contain max-w-none"
+                  />
+                </div>
+              ))}
+            </div>
+          ))}
+        </div>
+      </section>
+      <section
+        className="px-4
+        sm:px-6
         flex
-        after:content['']
-        after:dark:from-brand-dark
-        after:to-transparent
-        after:from-background
-        after:mg-gradient
-        "></div>
+        justify-center
+        items-center
+        flex-col
+        relative
+      "
+      >
+        <div
+          className="w-[30%]
+          blur-[120px]
+          rounded-full
+          h-32
+          absolute
+          bg-brand-primaryPurple/50
+          -z-10
+          top-22
+        "
+        />
+        <TitleSection
+          title="Keep track of your meetings all in one place"
+          subheading="Capture your ideas, thoughts, and meeting notes in a structured and organized manner."
+          pill="Features"
+        />
+        <div
+          className="mt-10
+          max-w-[450px]
+          flex
+          justify-center
+          items-center
+          relative
+          sm:ml-0
+          rounded-2xl
+          border-8
+          border-washed-purple-300 
+          border-opacity-10
+        "
+        >
+          <Image src={Cal} alt="Banner" className="rounded-2xl" />
+        </div>
       </section>
     </>
   );
