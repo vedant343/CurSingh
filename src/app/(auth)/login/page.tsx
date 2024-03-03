d6f537766ff808e12b19cb8adac7a564a91530d5
Vedant Bhalerao <vedantbhalerao343@gmail.com>
2024-03-03 13:10:43 +0530

    authorzation setup started

---
+++ src/app/(auth)/login/page.tsx
@@ -0,0 +1,19 @@
+import { FormSchema } from "@/lib/types";
+import { useRouter } from "next/router";
+import React, { useState } from "react";
+import { useForm } from "react-hook-form";
+import * as z from "zod";
+import { zodResolver } from "@hookform/resolvers/zod";
+
+const LoginPage = () => {
+  const router = useRouter();
+  const [submitError, setSubmitError] = useState("");
+
+  const form = useForm<Zod.infer<typeof FormSchema>>({
+    mode: "onChange",
+    resolver: zodResolver(FormSchema),
+  });
+  return <div>LoginPage</div>;
+};
+
+export default LoginPage;
