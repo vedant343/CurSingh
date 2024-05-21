07b91f9287d5922158080940b46e859a9e65026a
Vedant Bhalerao <vedantbhalerao343@gmail.com>
2024-05-22 02:18:09 +0530

    initial commit

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,6 +1,37 @@
-import { type ClassValue, clsx } from "clsx"
-import { twMerge } from "tailwind-merge"
+import { type ClassValue, clsx } from "clsx";
+import { twMerge } from "tailwind-merge";
+import { Price } from "./supabase/supabase.types";
 
 export function cn(...inputs: ClassValue[]) {
-  return twMerge(clsx(inputs))
+  return twMerge(clsx(inputs));
 }
+
+export const formatPrice = (price: Price) => {
+  const priceString = new Intl.NumberFormat("en-US", {
+    style: "currency",
+    currency: price.currency || undefined,
+    minimumFractionDigits: 0,
+  }).format((price?.unitAmount || 0) / 100);
+  return priceString;
+};
+
+export const postData = async ({
+  url,
+  data,
+}: {
+  url: string;
+  data?: { price: Price };
+}) => {
+  console.log("posting,", url, data);
+  const res: Response = await fetch(url, {
+    method: "POST",
+    headers: new Headers({ "Content-Type": "application/json" }),
+    credentials: "same-origin",
+    body: JSON.stringify(data),
+  });
+  if (!res.ok) {
+    console.log("Error in postData", { url, data, res });
+    throw Error(res.statusText);
+  }
+  return res.json();
+};
