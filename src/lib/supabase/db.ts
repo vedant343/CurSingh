73e796b2fd6c0e8191036eea38338f03e2925426
Vedant Bhalerao <vedantbhalerao343@gmail.com>
2024-06-16 11:23:00 +0530

    error handling in db.ts

--- src/lib/supabase/db.ts
+++ src/lib/supabase/db.ts
@@ -1,13 +1,8 @@
 import { drizzle } from "drizzle-orm/postgres-js";
-
 import postgres from "postgres";
-
 import * as dotenv from "dotenv";
-
-import * as schema from "../../../migrations/schema";
-
+import * as schema from "../../../migrations/schema"
 import { migrate } from "drizzle-orm/postgres-js/migrator";
-
 dotenv.config({ path: ".env" });
 
 if (!process.env.DATABASE_URL) {
@@ -19,21 +14,17 @@ const client = postgres(process.env.DATABASE_URL as string, {
   idle_timeout: 30,
   connect_timeout: 60,
 });
-
 const db = drizzle(client, { schema });
-
 const migrateDb = async () => {
   try {
     console.log("Migrating client");
-
     await migrate(db, { migrationsFolder: "migrations" });
-
     console.log("Successfully migrated");
+
   } catch (error) {
     console.log("Error migrating client");
   }
 };
-
 migrateDb();
 
 export default db;
